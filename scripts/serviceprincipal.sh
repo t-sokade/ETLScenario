3501094f03f17ee9fec9092d77724da2c192f334
Sophie Kader <52048866+t-sokade@users.noreply.github.com>
2019-08-15 09:23:43 -0700

    Update serviceprincipal.sh

--- scripts/serviceprincipal.sh
+++ scripts/serviceprincipal.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "Creating service principal"
+
 subscriptionId=$(az account show | jq -r '.id')
+
 az ad sp create-for-rbac --role "Storage Blob Data Contributor" --scope \
-    "subscriptions/$subscriptionId/resourceGroups/$resourceGroup/providers/Microsoft.Storage/storageAccounts/$ADLSGEN2StorageName" \
+    "subscriptions/$subscriptionId/resourceGroups/$resourceGroup/providers/Microsoft.Storage/storageAccounts/$ADLSGen2StorageName" \
     > serviceprincipal.json
